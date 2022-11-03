fab08930b27b61bfd12852734dfcada6df080330
mdjahidul1 <58892056+mdjahidul1@users.noreply.github.com>
2022-11-04 02:08:51 +0600

    Add files via upload

---
+++ set.js
@@ -0,0 +1,41 @@
+/*
+var mySet=new Set();
+mySet.add("bangladesh");
+mySet.add("india");
+mySet.add("Napal");
+mySet.add("bhutan");
+mySet.add("maldiv");
+mySet.add("china");
+mySet.add("india");
+mySet.add("bhutan");
+mySet.add("bangladesh");
+
+console.log(mySet);
+*/
+//********* */ set clear method***
+
+var mySet=new Set();
+mySet.add("bangladesh");
+mySet.add("india");
+mySet.add("Napal");
+mySet.add("bhutan");
+mySet.add("maldiv");
+mySet.add("china");
+mySet.add("india");
+mySet.add("bhutan");
+mySet.add("bangladesh");
+
+//  mySet.clear();
+// mySet.delete("india");
+// console.log(mySet.size);
+// console.log(mySet.values());
+
+// console.log(mySet);
+
+if(mySet.has("bangladesh")){
+    console.log("yes");
+}else{
+        console.log("no");
+}
+
+
