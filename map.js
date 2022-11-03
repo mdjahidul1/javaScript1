ebcba3ba617f95a7b8084eb14644be46cfa0574e
mdjahidul1 <58892056+mdjahidul1@users.noreply.github.com>
2022-11-04 01:27:06 +0600

    Add files via upload

---
+++ map.js
@@ -0,0 +1,12 @@
+var MyMap=new Map();
+MyMap.set("key1","Bangladesh")
+MyMap.set("key2","Bhutan")
+MyMap.set("key3","Sirilanka")
+MyMap.set("key4","Maldiv")
+MyMap.set("key5","Nepal")
+MyMap.set("key6","Thailand")
+
+// for(let myvalue of MyMap.values()){
+//     console.log(myvalue);
+// }
+console.log(MyMap.get("key4"));
