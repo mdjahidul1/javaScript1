5b99687e289dea0753cea6d5c40d96ce565eb71b
mdjahidul1 <58892056+mdjahidul1@users.noreply.github.com>
2022-11-04 02:29:43 +0600

    Add files via upload

---
+++ class.js
@@ -0,0 +1,60 @@
+
+/*class MyClass{
+
+  MyFun() {
+    
+    console.log("hlow Jahid")
+}
+
+MyFun1() {
+    
+    console.log("hlow Jahid 1")
+}
+
+MyFun2() {
+    
+    console.log("hlow Jahid 2")
+}
+
+MyFun3() {
+    
+    console.log("hlow Jahid 3")
+}
+
+}
+
+var obj=new MyClass;
+obj.MyFun1();
+obj.MyFun2();
+obj.MyFun3();
+obj.MyFun();
+*/
+class MyClass{
+
+    MyFun(name) {
+      
+      console.log(name)
+  }
+  
+  MyFun1(name) {
+      
+      console.log(name)
+  }
+  
+  MyFun2(name) {
+      
+      console.log(name)
+  }
+  
+  MyFun3(name) {
+      
+      console.log(name)
+  }
+  
+  }
+  
+  var obj=new MyClass;
+  obj.MyFun1("bangladesh");
+  obj.MyFun2("nepal");
+  obj.MyFun3("bhutan");
+  obj.MyFun("india");
