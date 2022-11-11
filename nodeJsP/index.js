5affac874a0776961068e25e00f9ee2ac1143d4b
mdjahidul1 <58892056+mdjahidul1@users.noreply.github.com>
2022-11-11 22:32:56 +0600

    Add files via upload

---
+++ nodeJsP/index.js
@@ -0,0 +1,15 @@
+var fs=require('fs');
+var http=require('http');
+var server=http.createServer(function(req,res){
+   if(req.url="/"){
+    
+    fs.readFile('home.html',function(error,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        res.end();
+    });
+   }
+
+});
+server.listen(5050);
+console.log("server is run success");
